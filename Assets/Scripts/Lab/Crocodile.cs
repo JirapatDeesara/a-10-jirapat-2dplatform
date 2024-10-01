db9f69da62d1b93e8dfbb73adce64109d8266346
JirapatDeesara <jirapat.dees@bumail.net>
2024-10-01 11:14:56 +0700

    Crocodile can shoot

--- Assets/Scripts/Lab/Crocodile.cs
+++ Assets/Scripts/Lab/Crocodile.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class Crocodile : Enemy // แดงเพราะติด Abstract
 {
-    // -attackRange: float
-    private float attackRange;
+    [SerializeField]private float attackRange;
     // -player: Player
-    private Player player;
+    [SerializeField]private Player player;
+    // สร้างกระสุน
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform  bulletSpawnPoint;
+    [SerializeField] private float bulletWaitTime;
+    [SerializeField] private float bulletTimer;
     //แก้แดง
     // ใส่ Private void Start ใน class ลูกทุกตัว
     private void Start()
     {
         Init(100);
-        Debug.Log(Health);
-        Behaviour();
+        
     }
+
+    private void FixedUpdate()
+    {
+        // นับเวลาถอยหลัง Cooldown
+        bulletTimer -= Time.deltaTime;
+        Behaviour();
+
+        if (bulletTimer < 0)
+        {
+            bulletTimer = bulletWaitTime;
+        }
+    }// End Crocodile FixedUpdate
     public override void Behaviour()
     {
-        Debug.Log("Crocodile.Behaviour");
+        Vector2 direction = player.transform.position - transform.position;
+        float distance = direction.magnitude;
+        //  เช็คระยะโจมตี Attack range
+        if (distance < attackRange)
+        {
+            Shoot();
+        }
     }// End Override
+    private void Shoot()
+    {
+        if (bulletTimer < 0)
+        {
+            bullet = Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
+        } // End if bulletTimer
+       
+    }
 }// End Crocodile
